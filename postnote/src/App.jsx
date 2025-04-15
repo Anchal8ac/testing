2da1ef5e8f3a25d5c38a69ae82a6eb08f3e22a44
Anchal8ac <anchalchauhan081@gmail.com>
2025-04-15 17:29:43 +0530

    app

--- postnote/src/App.jsx
+++ postnote/src/App.jsx
@@ -3,19 +3,27 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Layout from "./pages/Layout";
 import LoginPage from "./pages/loginPage";
 import HomePage from "./pages/homePage";
-import Project from "./Pages/Project";
+import Project from "./pages/Project";
 import AccountPage from "./pages/AccountPage";
+import PrivateRoute from "./pages/ProtectRoute"; 
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Layout />}>
-        <Route index element={<HomePage />} />
-        <Route path="home" element={<HomePage/>} />
-          <Route path="login" element={<LoginPage/>} />
-          <Route path="account" element={<AccountPage/>} />
-          <Route path="project" element={<Project />} />
+          <Route index element={<HomePage />} />
+          <Route path="home" element={<HomePage />} />
+          <Route path="login" element={<LoginPage />} />
+          <Route path="account" element={<AccountPage />} />
+          <Route
+            path="project"
+            element={
+              <PrivateRoute>
+                <Project />
+              </PrivateRoute>
+            }
+          />
         </Route>
       </Routes>
     </Router>
